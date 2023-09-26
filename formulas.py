fdeb7cb3e7e0c6cf8589a7d9e22d3fdcfa99c5f7
Fatima <fatima.hammouri@gmail.com>
2023-09-26 12:36:39 -0700

    Temperature Conversion Program

---
+++ formulas.py
@@ -0,0 +1,15 @@
+def celsius_to_fahrenheit():
+    pass
+def celsius_to_kelvin():
+    pass
+def fahrenheit_to_celsius():
+    pass
+def fahrenheit_to_kelvin():
+    pass
+def kelvin_to_fahrenheit():
+    pass
+    pass
+def kelvin_to_celsius():
+    pass
+
+
