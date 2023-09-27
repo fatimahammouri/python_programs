0c536eeb4fbd942bc3917508b18a312e2c28722d
Fatima <fatima.hammouri@gmail.com>
2023-09-27 11:07:55 -0700

    optimize

--- prompts.py
+++ prompts.py
@@ -1,7 +1,11 @@
-"""
-Helper function to calculate temperature using conversion formulas
-depending on the user-inputted units
-"""
+# Boolean that will keep the program running until user inputs E
+show_calculater = True
+
+# TO DO:
+# optimize choose formula function to reduce redundancy
+
+# Helper function to calculate temperature using conversion formulas
+# depending on the user-inputted units
 def choose_formula(from_unit, to_unit, temp):
     # case: if the user inpts same unit for FROM and TO units
     if from_unit.upper() == to_unit.upper():
@@ -29,25 +33,26 @@ def choose_formula(from_unit, to_unit, temp):
 
     # case: if the user inputs from kelvin to fahrenheit
     elif from_unit in ["K", "k"] and to_unit in ["F", "f"]:
-        f_temp =  temp * 9 / 5 - 459.67
-        print(f"{temp:.2f}{from_unit.upper()} = {f_temp:.2f}{to_unit.upper()}")
+        fahrenheit_temp =  temp * 9 / 5 - 459.67
+        print(f"{temp:.2f}{from_unit.upper()} = {fahrenheit_temp:.2f}{to_unit.upper()}")
 
     # case: if the user inputs from kelvin to celsius   
     elif from_unit in ["K", "k"] and to_unit in ["c", "C"]:
-        c_temp = temp - 273.15
-        print(f"{temp:.2f}{from_unit.upper()} = {c_temp:.2f}{to_unit.upper()}")
+        celsius_temp = temp - 273.15
+        print(f"{temp:.2f}{from_unit.upper()} = {celsius_temp:.2f}{to_unit.upper()}")
+
 
 # Exit program function to exit any time user inputs "E"
 def exit_program():
-    print("Exiting the temperature conversion calculator.")
+    print("\nExiting the temperature conversion calculator.")
     show_calculater = False
     exit()
 
+# Calculator program and prompts
 def enter_calculater():
     print("This is a temperature unit conversion calculator.\nIt converts from/to Celsius, Fahrenheit, and Kelvin.\n \n")
 
     temp_prompt = input("Enter a temperature(number), or E to exit: ")
-
     if temp_prompt == "E":
         exit_program()
 
@@ -57,7 +62,7 @@ def enter_calculater():
     second_move = False
 
     while first_move:
-        from_unit = input("Enter FROM temperature unit :[Cc/Ff/Kk]")
+        from_unit = input("Enter FROM temperature unit:[Cc/Ff/Kk]")
         if from_unit == "E":
             exit_program()
         if from_unit in ["C", "c", "F", "f", "K", "k"]:
@@ -67,7 +72,7 @@ def enter_calculater():
             print(f"[ERROR] Invalid temperature unit {from_unit}\nValid choices are:[Cc/Ff/Kk]")
 
     while second_move:
-        to_unit = input("Enter TO temperature unit :[Cc/Ff/Kk]")
+        to_unit = input("Enter TO temperature unit:[Cc/Ff/Kk]")
         if to_unit == "E":
             exit_program()
         if to_unit in ["C", "c", "F", "f", "K", "k"]:
@@ -75,10 +80,7 @@ def enter_calculater():
             second_move = False
         else:
             print(f"[ERROR] Invalid temperature unit {from_unit}\nValid choices are:[Cc/Ff/Kk]")
-    
-show_calculater = True
+
+# as long as user didn't input E keep running calculator program
 while show_calculater:
     enter_calculater()
-
-
-
